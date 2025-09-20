adee015e6478f226a1ec5a1db2493226acb5655b
ConstanzaCif <117744459+ConstanzaCif@users.noreply.github.com>
2025-09-19 22:48:24 -0600

    Correccion de script + controller usuarios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const usuario_routes = require('./usuario_routes');
+
 // const exampleRouter = require('./exampleRouter');
 
 // router.use('/example', exampleRouter);
+
+router.use('/usuarios', usuario_routes);
 
 
 router.get('/', (req, res) => {
